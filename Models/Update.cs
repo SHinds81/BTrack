f2b338d503c0fa0a073d893a4224019e52afc745
Sean Hinds <seanhinds81@gmail.com>
2023-11-02 16:20:54 +1100

    Create Update.cs

---
+++ Models/Update.cs
@@ -0,0 +1,11 @@
+﻿namespace BTrack.Models
+
+{   
+    public class Update
+    {
+        public int bugId {  get; set; }
+        public int accountId { get; set; }
+        public DateTime updateDate { get; set; }
+        public string updateDetails { get; set; }
+    }
+}
