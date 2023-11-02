e4d10e910d48fb202c6ed2ee1107f202b64d106d
Sean Hinds <seanhinds81@gmail.com>
2023-11-02 16:04:31 +1100

    Create Bugs.cs

---
+++ Models/Bugs.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTrack.Models
+{
+    public class Bugs
+    {
+        public int bugId { get; set; }
+        public int bugStatus { get; set; }
+        public string bugTitle { get; set; }
+        public string bugDescription { get; set;}
+        public int bugPriority { get; set; }
+    }
+}
