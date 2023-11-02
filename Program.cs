e81f302b18f8fab9d1f342c504231241cbc8118d
Sean Hinds <seanhinds81@gmail.com>
2023-11-02 22:04:30 +1100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using BTrack.Data;
+using BTrack.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BTrackContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("BTrackContext") ?? throw new InvalidOperationException("Connection string 'BTrackContext' not found.")));
@@ -9,6 +11,13 @@ builder.Services.AddDbContext<BTrackContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
