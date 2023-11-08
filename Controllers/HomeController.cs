0b5fbf18d51e3e80aacf2a4ca4919ca1582a9def
Sean Hinds <seanhinds81@gmail.com>
2023-11-09 06:17:09 +1100

    front page counting start

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,25 +2,41 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
+using BTrack.Data;
+using BTrack.Models;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
 namespace BTrack.Controllers
 {
     public class HomeController : Controller
     {
 
         public string testString2 = "TestString2";
+        string testString = "Teststring";
+
+        private readonly BTrackContext _context;
 
         private readonly ILogger<HomeController> _logger;
 
-        string testString = "Teststring";
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BTrackContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             
+            //var recordCount = ctx.Cases.Count(a => a.Role == "admin");
+            var bugStatus1Total = _context.Bugs.Count(a => a.bugStatus == 1);
+            var bugStatus2Total = _context.Bugs.Count(a => a.bugStatus == 2);
+            var bugStatus3Total = _context.Bugs.Count(a => a.bugStatus == 3);
+
+            ViewData["bugStatus1Total"] = bugStatus1Total;
+            ViewData["bugStatus2Total"] = bugStatus2Total;
+            ViewData["bugStatus3Total"] = bugStatus3Total;
+
+
             ViewData["TestDisplay"] = testString;
             ViewData["TestDisplay2"] = testString2;
 
