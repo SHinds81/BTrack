5491de345f4778eca7c768df38a611952790dbc7
Sean Hinds <seanhinds81@gmail.com>
2023-12-18 18:02:43 +1100

    starting login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BTrack.Data;
+using BTrack.Models;
+
+namespace BTrack.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly BTrackContext _context;
+
+        public LoginController(BTrackContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Login
+        public async Task<IActionResult> Index()
+        {
+            return _context.Login != null ?
+                        View(await _context.Login.ToListAsync()) :
+                        Problem("Entity set 'BTrackContext.Login'  is null.");
+        }
+
+    }
+
+}
